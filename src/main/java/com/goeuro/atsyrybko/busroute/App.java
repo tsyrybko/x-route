170120ac9b45e23d0840348a56a8e9bacbd832ac
Arseni Tsyrybko <a.tsyrybko@godeltech.com>
2016-10-31 19:38:54 +0300

    refactoring

--- src/main/java/com/goeuro/atsyrybko/busroute/App.java
+++ src/main/java/com/goeuro/atsyrybko/busroute/App.java
@@ -8,14 +8,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
 /**
  * Created by arseni on 30.10.16.
@@ -45,14 +43,12 @@ public class App {
 
     public static void readFile(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader/*("./txt/test.txt"));*/(/*args[0]*/"./txt/route_data_large.txt"));
-        Integer qq = Integer.valueOf(br.readLine().split(" ")[0]);
+        Integer qq = Integer.parseInt(br.readLine().split(" ")[0]);
 
         for (int i=0; i< qq;i++) {
             System.out.println("Route : " + i);
 
-            TreeSet<Integer> set = new TreeSet<>(Arrays.<Integer>asList(Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray()));
-
-            SOURCE_LIST.add(set);
+            SOURCE_LIST.add(getRoute(br.readLine()));
 
         }
 
@@ -61,8 +57,17 @@ public class App {
 
 
         System.out.print("********** PARSED ************");
+    }
 
+    private static Set<Integer> getRoute(String line) {
+        String[] routeArray = line.split(" ");
 
+        Set result = new HashSet(routeArray.length);
+        for (int i=1; i<routeArray.length; i++) {
+            Integer element = Integer.parseInt(routeArray[i]);
+            result.add(element);
+        }
 
+        return result;
     }
 }
