2311ff77d156b6ad370498fd66a071f971dc3c5d
Arseni Tsyrybko <a.tsyrybko@godeltech.com>
2016-11-01 12:56:22 +0300

    refactoring

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ bus routes
 
 Service was developed according to KISS principle.
 
-Assumption: all routes bi-directional
+Assumption: all routes are bidirectional
+
+Note: Aim of the project - demonstrate possibility store in memory all needed data,
+      start service and provide response in reasonable time.
+      It is still necessary to add some checks and javadocs,
+      improve exception handling, logging and test coverage for production purpose.
--- src/main/java/com/goeuro/atsyrybko/busroute/App.java
+++ src/main/java/com/goeuro/atsyrybko/busroute/App.java
@@ -2,6 +2,8 @@ package com.goeuro.atsyrybko.busroute;
 
 import com.goeuro.atsyrybko.busroute.service.RoutesParsingService;
 import com.goeuro.atsyrybko.busroute.service.ServerInitialisationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -9,8 +11,14 @@ import java.io.IOException;
  * Main application class
  */
 public class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RoutesParsingService.class);
 
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        if (args == null || args.length == 0 || args[0].isEmpty()) {
+            LOGGER.error("Please define file name as a parameter in command line and try again.");
+            return;
+        }
 
         RoutesParsingService routesParsingService = new RoutesParsingService();
         routesParsingService.readFile(args[0]);
